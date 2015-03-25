6816f1263bb420b766bb9c50ff772986f50e99fd
Alvaro Sanchez <alvarom.se@gmail.com>
2015-03-25 23:21:08 +0100

    Add 4 pomodoros cycle script
    Loops through four pomodoros, including 5 minutes short brakes.
    
    In the end a notification pops up to let the user know a long
    brake can be taken now. It also asks the user to restart the
    script if she wants another cycle.
    
    The script uses zenity for the popups.

---
+++ pomodoro.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+COUNTER=0
+while [ $COUNTER -lt 4 ]; do
+    let P=COUNTER+1
+    ./p-timer.sh 1 "Pomodoro $P/4" "Pomodoro started, you have 25 minutes" "Pomodoro ended." 
+    ./p-timer.sh 1 "Brake" "Short brake started (5 minutes)" "Brake ended.\nClick Yes to continue with the next Pomodoro"
+    if zenity --question --text "Continue?"; then
+        echo "Ok, otro pomodoro"
+        let COUNTER=COUNTER+1
+    else
+        exit
+    fi
+    sleep 2
+done
+zenity --info --text="You've completed 4 pomodoros,\ntake a long brake (15-30 minutes).\n\nThen you can restart this cycle (it's not automatic)."
