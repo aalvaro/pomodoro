c63aae3cf57846101222e6455191cab0967a9b97
Alvaro Sanchez <alvarom.se@gmail.com>
2015-03-26 02:49:40 +0100

    Fix #4 : Fourth pomodoro brake
    Also changed minutes of the timers to 25 and 5 minutes

--- pomodoro.sh
+++ pomodoro.sh
@@ -5,14 +5,16 @@ echo $MY_PATH
 COUNTER=0
 while [ $COUNTER -lt 4 ]; do
     let P=COUNTER+1
-    $MY_PATH/p-timer.sh 1 "Pomodoro $P/4" "Pomodoro started, you have 25 minutes" "Pomodoro ended." 
-    $MY_PATH/p-timer.sh 1 "Brake" "Short brake started (5 minutes)" "Brake ended.\nClick Yes to continue with the next Pomodoro"
-    if zenity --question --text "Continue?"; then
-        echo "Ok, otro pomodoro"
-        let COUNTER=COUNTER+1
-    else
-        exit
+    $MY_PATH/p-timer.sh 25 "Pomodoro $P/4" "Pomodoro started, you have 25 minutes" "Pomodoro ended." 
+    if [ $COUNTER -lt 3 ]; then
+        $MY_PATH/p-timer.sh 5 "Brake" "Short brake started (5 minutes)" "Brake ended.\nClick Yes to continue with the next Pomodoro"
+    
+        if zenity --question --text "Continue with another Pomodoro?\n\n(You have completed $P out of 4)"; then
+            echo "Ok, otro pomodoro"
+        else
+            exit
+        fi
     fi
-    sleep 2
+    let COUNTER=COUNTER+1
 done
 zenity --info --text="You've completed 4 pomodoros,\ntake a long brake (15-30 minutes).\n\nThen you can restart this cycle (it's not automatic)."
